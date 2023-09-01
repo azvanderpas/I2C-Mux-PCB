10758a7902dec56cf091f49c4727ebe8bbace342
azvanderpas <azvanderpas@gmail.com>
2023-09-01 03:40:50 +0000

    YESgit stage .devcontainer/*

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,5 +2,5 @@
 sudo add-apt-repository -y ppa:kicad/kicad-7.0-releases
 sudo apt update -y
 sudo apt install kicad -y
-sudo apt install matchbox -y
+DEBIAN_FRONTEND=noninteractive sudo apt install matchbox -y
 sudo apt upgrade -y
