11c861475ee6b508b8ef43beda83bad8b35bf57b
azvanderpas <azvanderpas@gmail.com>
2023-09-01 03:13:45 +0000

    Yes

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-sudo add-apt-repository ppa:kicad/kicad-7.0-releases
+sudo add-apt-repository -y ppa:kicad/kicad-7.0-releases
 sudo apt update
 sudo apt install kicad
 sudo apt install matchbox
+sudo apt upgrade -y
