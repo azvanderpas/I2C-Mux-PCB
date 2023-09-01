186901d6317887ec69656e57673956d445f4b504
azvanderpas <azvanderpas@gmail.com>
2023-09-01 04:03:03 +0000

    YESSS!

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo add-apt-repository -y ppa:kicad/kicad-7.0-releases
 sudo apt update -y
+sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration -y
 sudo apt install kicad -y
-sudo DEBIAN_FRONTEND=noninteractive apt install matchbox -y
+sudo apt install matchbox -y
 sudo apt upgrade -y
